7ccad7e300d5a59ab2aab205818e872a072438f1
UncleChange <1071046531@qq.com>
2015-03-09 15:36:55 +0800

    音乐播放器bug修复

--- media/js/jia.js
+++ media/js/jia.js
@@ -4,12 +4,14 @@ $("#listplayer").mouseout(function(){
     if(flag){
         $("#playerobj").css("display","none");
         $("#listplayer").css("overflow","hide");
+        $("#listplayer").css("z-index","-100000000");
     }
 });
 $("#playbutton").mouseout(function(){
     if(flag){
         $("#playerobj").css("display","none");
         $("#listplayer").css("overflow","hide");
+        $("#listplayer").css("z-index","-100000000");
     }
 });
 })
@@ -17,9 +19,11 @@ $("#playbutton").mouseout(function(){
 function playerOver(){
     $("#listplayer").css("overflow","auto");
     $("#playerobj").css("display","inline");
+    $("#listplayer").css("z-index","100000000");
 }
 function playerClick(){
     flag=!flag;
     $("#listplayer").css("overflow","auto");
     $("#playerobj").css("display","inline");
+    $("#listplayer").css("z-index","100000000");
 }
