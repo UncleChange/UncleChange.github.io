01c25a9cd32667c1ef0b1b40f10181ed0c502b0c
UncleChange <1071046531@qq.com>
2015-04-09 16:18:57 +0800

    触摸事件

--- media/js/jia.js
+++ media/js/jia.js
@@ -27,19 +27,24 @@ function playerClick(){
     $("#playerobj").css("display","inline");
     $("#listplayer").css("z-index","100000000");
 }
-
-  window.onload=function(){ 
-        function gesture(ev){ 
-            var div=document.getElementById('playbutton'); 
+ window.onload=function(){ 
+   function touches(ev){ 
+        if(ev.touches.length==1){ 
             switch(ev.type){ 
                 case 'touchstart': 
                     $("#listplayer").css("overflow","auto");
                     $("#playerobj").css("display","inline");
-                    $("#listplayer").css("z-index","100000000"); 
+                    $("#listplayer").css("z-index","100000000");
                     break; 
+                case 'touchend': 
+                    break; 
+                case 'touchmove': 
+                    break; 
+                 
             } 
         } 
-         document.addEventListener('touchstart',touches,false); 
+    } 
+    document.addEventListener('touchstart',touches,false); 
     document.addEventListener('touchend',touches,false); 
     document.addEventListener('touchmove',touches,false);  
     } 
